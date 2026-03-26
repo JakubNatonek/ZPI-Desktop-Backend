317c2d5af862b26e0d91042b7f5c11fa33443203
Jakub Natonek <natonek32@gmail.com>
2026-03-26 17:23:45 +0100

    feat(room_type): implement CRUD operations and seeding for room types

--- app/seed_data/seed_all.py
+++ app/seed_data/seed_all.py
@@ -5,6 +5,7 @@ from app.core.database import SessionLocal
 from app.seed_data.seed_admin import seed_admin
 from app.seed_data.seed_departments import seed_departments
 from app.seed_data.seed_roles import seed_roles
+from app.seed_data.seed_room_type import seed_room_types
 from app.seed_data.rapla.seed_rapla_all import seed_rapla_all
 
 
@@ -14,6 +15,7 @@ def seed_all() -> None:
         # for app
         seed_departments(db)
         seed_roles(db)
+        seed_room_types(db)
         admin_id: int = seed_admin(db)
 
         #for rapla
