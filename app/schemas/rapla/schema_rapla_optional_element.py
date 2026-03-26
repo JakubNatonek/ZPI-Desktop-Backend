49e408c2af65822bace0f029e9de3b0c78da8e63
Jakub Natonek <natonek32@gmail.com>
2026-03-26 17:00:37 +0100

    feat(rapla): update OptionalElement to support optional fields and improve XML serialization

--- app/schemas/rapla/schema_rapla_optional_element.py
+++ app/schemas/rapla/schema_rapla_optional_element.py
@@ -1,21 +1,23 @@
 from dataclasses import dataclass, field
 import xml.etree.ElementTree as ET
-from typing import cast, List
+from typing import cast, List, Optional
 
 from app.schemas.rapla.rapla_namespaces import RELAXNG_NS, RAPLA_NS
 
 from app.schemas.rapla.schema_rapla_language_name import RaplaLanguageName
 from app.schemas.rapla.schema_rapla_data_type import RaplaDataType
 from app.schemas.rapla.schema_rapla_constraint import RaplaConstraint
+from app.schemas.rapla.schema_rapla_annotations import RaplaAnnotations
 
 
 @dataclass
 class OptionalElement:
     name: str
-    default_value: str
     data_type: RaplaDataType
+    default_value: Optional[str] = None
     names: List[RaplaLanguageName] = field(default_factory=lambda: cast(List[RaplaLanguageName], []))
-    constraints: List[RaplaConstraint] = field(default_factory=lambda: cast(List[RaplaConstraint], []))
+    constraints: Optional[List[RaplaConstraint]] | None = field(default_factory=lambda: cast(List[RaplaConstraint], []))
+    annotations: Optional[RaplaAnnotations] | None = field(default_factory=lambda: RaplaAnnotations())
 
     def to_xml(self, parent: ET.Element) -> ET.Element:
         optional_element = ET.SubElement(
@@ -30,19 +32,25 @@ class OptionalElement:
         for name in self.names:
             name.to_xml(optional_element)
 
+
+        if self.annotations is not None:
+            self.annotations.to_xml(optional_element)
+
         # type of data
         self.data_type.to_xml(optional_element)
 
-        # constrains
-        for constraint in self.constraints:
-            constraint.to_xml(optional_element)
+        # constraints
+        if self.constraints is not None:
+            for constraint in self.constraints:
+                constraint.to_xml(optional_element)
 
 
-        # defoult value
-        default_value_element = ET.SubElement(
-            optional_element, 
-            f"{{{RAPLA_NS}}}default",
-        )
-        default_value_element.text = self.default_value
+        # default value
+        if self.default_value is not None:
+            default_value_element = ET.SubElement(
+                optional_element, 
+                f"{{{RAPLA_NS}}}default",
+            )
+            default_value_element.text = self.default_value
 
         return optional_element
