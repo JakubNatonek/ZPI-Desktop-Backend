172424a9ca2cfae6cdbd220fcee4f0e30027b9b3
Jakub Natonek <natonek32@gmail.com>
2026-05-11 20:02:17 +0200

    Merg-1-INO-tst

--- app/seed_data/seed_model/__init__.py
+++ app/seed_data/seed_model/__init__.py
@@ -14,6 +14,7 @@ from app.seed_data.seed_model.seed_titles import seed_titles
 from app.seed_data.seed_model.seed_semesters import seed_semesters
 from app.seed_data.seed_model.seed_subjects import seed_subjects
 from app.seed_data.seed_model.seed_lessons import seed_lessons
+from app.seed_data.seed_model.seed_groups import seed_groups
 from app.seed_data.seed_model.seed_teacher_student_profiles import seed_teacher_student_profiles
 from app.seed_data.seed_model.seed_thesis import seed_thesis
 from app.seed_data.seed_model.seed_thesis_proposals import seed_thesis_proposals
@@ -40,6 +41,7 @@ __all__ = [
 	"seed_semesters",
 	"seed_subjects",
 	"seed_lessons",
+	"seed_groups",
 	"seed_teacher_student_profiles",
 	"seed_thesis_settings",
 	"seed_thesis_proposals",
